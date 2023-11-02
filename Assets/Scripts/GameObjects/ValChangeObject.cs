6a61cd3211cd250cd71e3d97a94cb0fd73198181
Adlet <eypk777@gmail.com>
2023-11-03 02:12:48 +0900

    Add Textmeshpro package, UI Components

---
+++ Assets/Scripts/GameObjects/ValChangeObject.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ValChangeObject : MonoBehaviour
+{
+    [SerializeField] private String suffix;
+    [SerializeField] private TextMeshProUGUI currentValue;
+    [SerializeField] private TextMeshProUGUI changedValue;
+    
+    public void UpdateValue(int addValue)
+    {
+        currentValue.text = Int32.Parse(currentValue.text + addValue).ToString();
+        currentValue.text += suffix;
+        if (addValue > 0)
+        {
+            changedValue.text = "+";
+            changedValue.text += addValue.ToString();
+            changedValue.color = Color.red;
+        }
+        else if (addValue < 0)
+        {
+            changedValue.text = "-";
+            changedValue.text += addValue.ToString();
+            changedValue.color = Color.blue;
+        }
+        else
+        {
+            changedValue.text = "-";
+            changedValue.color = Color.black;
+        }
+    }
+}
